ae9c4434f9cb7f80d2df02e591f2ae5cbaea5659
EBS\igor.cuiumju <igori1992laky@gmail.com>
2018-01-30 17:11:14 +0200

    Add atriburt to a

--- General/Menu.js
+++ General/Menu.js
@@ -2,7 +2,10 @@ function addItem() {
       var node = document.createElement("a");
       var x = document.getElementById("add").value;
       var text = document.createTextNode(x);
+
+      node.setAttribute('href', "#");
       node.appendChild(text);
       document.getElementsByClassName("dropdown-content")[0].appendChild(node);
       document.getElementById("add").value="";
+      localStorage.setItem("text", someVarName);
 }
