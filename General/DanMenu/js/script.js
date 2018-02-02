20bed264222a1c6c19f92c5f5779b2ef5862bfce
Dan <dan>
2018-02-02 13:30:55 +0200

    update dan menu

--- General/DanMenu/js/script.js
+++ General/DanMenu/js/script.js
@@ -1,8 +1,27 @@
-function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.className === "topnavbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnavbar";
-    }
-}
+(function($){
+
+  function myFunction() {
+      var x = document.getElementById("navbar");
+      if (x.className === "topnavbar") {
+          x.className += " responsive";
+      } else {
+          x.className = "topnavbar";
+      }
+  }
+
+  $(document).ready(function () {
+    $.ajax({
+      url: "http://localhost:3000/posts",
+      type: "GET",
+      cache: false,
+      success: function(data){
+        for( var i=1; i<=data.length; i++){
+          $('.topnavbar').append('<a>Text</a>');
+          $('.topnavbar a:nth-child('+(i+1)+')').text(data[i-1].name);
+          $('.topnavbar a:nth-child('+(i+1)+')').attr("href", data[i-1].url);
+        }
+      }
+    });
+  });
+
+})(jQuery);
